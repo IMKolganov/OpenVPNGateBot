642c4cb1089bee0f94990808c67e856d0dc18ee1
Ivan Kolganov <imkolganov@gmail.com>
2025-04-11 04:52:39 +0300

    added LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile

--- DataGateVPNBot.DataBase/Migrations/20250411015217_LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile.Designer.cs
+++ DataGateVPNBot.DataBase/Migrations/20250411015217_LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataGateVPNBot.DataBase.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataGateVPNBot.DataBase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250411015217_LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile")]
+    partial class LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataGateVPNBot.DataBase/Migrations/20250411015217_LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataGateVPNBot.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocalizationTextSeedData_SomethingWentWrongWhenTryMakeNewFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "xgb_botvpndev",
+                table: "LocalizationTexts",
+                columns: new[] { "Id", "Key", "Language", "Text" },
+                values: new object[,]
+                {
+                    { 64, "SomethingWentWrongWhenTryMakeNewFile", 1, "Something went wrong while trying to create a new file." },
+                    { 65, "SomethingWentWrongWhenTryMakeNewFile", 3, "Произошла ошибка при попытке создать новый файл." },
+                    { 66, "SomethingWentWrongWhenTryMakeNewFile", 2, "Κάτι πήγε στραβά κατά την προσπάθεια δημιουργίας νέου αρχείου." }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "xgb_botvpndev",
+                table: "LocalizationTexts",
+                keyColumn: "Id",
+                keyValue: 64);
+
+            migrationBuilder.DeleteData(
+                schema: "xgb_botvpndev",
+                table: "LocalizationTexts",
+                keyColumn: "Id",
+                keyValue: 65);
+
+            migrationBuilder.DeleteData(
+                schema: "xgb_botvpndev",
+                table: "LocalizationTexts",
+                keyColumn: "Id",
+                keyValue: 66);
+        }
+    }
+}
