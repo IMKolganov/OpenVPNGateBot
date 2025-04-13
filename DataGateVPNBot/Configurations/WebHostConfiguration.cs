f52adc42787f3a932d025db8d4ad5a7cdf997d94
Ivan Kolganov <imkolganov@gmail.com>
2025-04-14 02:38:33 +0300

    show version when started app

--- DataGateVPNBot/Configurations/WebHostConfiguration.cs
+++ DataGateVPNBot/Configurations/WebHostConfiguration.cs
@@ -1,13 +1,16 @@
-﻿namespace DataGateVPNBot.Configurations;
+﻿using ILogger = Serilog.ILogger;
+
+namespace DataGateVPNBot.Configurations;
 
 public static class WebHostConfiguration
 {
-    public static void ConfigureWebHost(this WebApplicationBuilder builder)
+    public static void ConfigureWebHost(this WebApplicationBuilder builder, ILogger logger)
     {
         var config = builder.Configuration;
 
-        var certPath = config["CERTIFICATE_PATH"]; 
+        var certPath = config["CERTIFICATE_PATH"];
         var portStr = config["TELEGRAMBOT_PORT"];
+        var port = int.TryParse(portStr, out var parsedPort) ? parsedPort : 8443;
 
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
@@ -15,12 +18,25 @@ public static class WebHostConfiguration
 
             if (!string.IsNullOrWhiteSpace(certPath))
             {
-                var port = int.TryParse(portStr, out var parsedPort) ? parsedPort : 8443;
-
-                serverOptions.ListenAnyIP(port, listen =>
+                if (File.Exists(certPath))
                 {
-                    listen.UseHttps(certPath);
-                });
+                    logger.Information($"🔐 HTTPS certificate found at: {certPath}");
+
+                    serverOptions.ListenAnyIP(port, listen =>
+                    {
+                        listen.UseHttps(certPath);
+                    });
+                }
+                else
+                {
+                    logger.Warning($"⚠ Certificate file '{certPath}' not found. Falling back to HTTP only.");
+                    serverOptions.ListenAnyIP(port);
+                }
+            }
+            else
+            {
+                logger.Warning("⚠ CERTIFICATE_PATH not provided. Running HTTP only.");
+                serverOptions.ListenAnyIP(port);
             }
         });
     }
--- DataGateVPNBot/Program.cs
+++ DataGateVPNBot/Program.cs
@@ -1,15 +1,20 @@
+using System.Reflection;
 using DataGateVPNBot.Configurations;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 builder.Host.ConfigureSerilog();
+var logger = Log.ForContext("SourceContext", "DILogger");
+var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown version";
+logger.Information($"Application version: {version};");
 
 builder.Services.ConfigureTelegram(builder.Configuration);
 builder.Services.ConfigureServices();
 builder.Services.DataBaseServices(builder.Configuration);
 builder.Services.ConfigureDashboardApi();
 
-builder.ConfigureWebHost();
+builder.ConfigureWebHost(logger);
 
 var app = builder.Build();
 
