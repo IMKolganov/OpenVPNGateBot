5e204f6c652f9bdbe31ab4df091a9040d5a2b61d
Ivan Kolganov <imkolganov@gmail.com>
2025-04-17 00:46:25 +0300

    fixed user service

--- DataGateVPNBot/Services/DashboardServices/TelegramBotUserService.cs
+++ DataGateVPNBot/Services/DashboardServices/TelegramBotUserService.cs
@@ -1,30 +1,90 @@
-using System.Text.Json;
+using System.Security.Authentication;
 using DataGateVPNBot.Services.Http;
+using OpenVPNGateMonitor.SharedModels.Responses;
+using OpenVPNGateMonitor.SharedModels.TelegramBotUser.Requests;
+using OpenVPNGateMonitor.SharedModels.TelegramBotUser.Responses;
 
 namespace DataGateVPNBot.Services.DashboardServices;
 
 public class TelegramBotUserService
 {
     private readonly IHttpRequestService _httpRequestService;
-    private readonly RedisCacheService _redisCacheService;
-    private readonly string _clientId;
-    private readonly string _clientSecret;
-    private readonly ILogger<AuthService> _logger;
+    private readonly ILogger<TelegramBotUserService> _logger;
+    private readonly AuthService _authService;
+    private const string EndpointRegisterUser = "api/TelegramBotUser/RegisterUser";
+    private const string EndpointGetAdmins = "api/TelegramBotUser/GetAdmins";
 
-    private const string TokenCacheKey = "dashboard_openvpn_token";
-    private readonly TimeSpan _tokenExpiration = TimeSpan.FromHours(1);
-
-    public TelegramBotUserService(
+    public TelegramBotUserService(ILogger<TelegramBotUserService> logger,
         IHttpRequestService httpRequestService,
-        RedisCacheService redisCacheService,
-        string clientId,
-        string clientSecret,
-        ILogger<AuthService> logger)
+        AuthService authService)
     {
-        _httpRequestService = httpRequestService;
-        _redisCacheService = redisCacheService;
-        _clientId = clientId;
-        _clientSecret = clientSecret;
         _logger = logger;
+        _httpRequestService = httpRequestService;
+        _authService = authService;
+    }
+
+    public async Task<RegisterUserResponse> RegisterUserAsync(RegisterUserRequest request, 
+        CancellationToken cancellationToken)
+    {
+        if (request.TelegramId <= 0) 
+            throw new ArgumentException("TelegramId is required.");
+        
+        var token = await _authService.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+        {
+            throw new AuthenticationException("Authentication failed. Failed to obtain a valid token from API.");
+        }
+        
+        _logger.LogInformation($"Sending request to create TelegramBotUser TelegramId: {request.TelegramId}");
+
+        var response =
+            await _httpRequestService.PostAsync<ApiResponse<RegisterUserResponse>>(EndpointRegisterUser, request, 
+                token, cancellationToken);
+
+        if (response is { Success: true, Data: not null })
+        {
+        }
+        else
+        {
+            _logger.LogWarning($"Failed to RegisterUserAsync: {response?.Message}");
+        }
+
+        if (response == null)
+        {
+            _logger.LogError("Failed to RegisterUserAsync.");
+        }
+
+        return response!.Data!;
+    }
+
+    public async Task<GetAdminsResponse> GetAdminsAsync(CancellationToken cancellationToken)
+    {
+        var telegramBotAdmins = new GetAdminsResponse();
+        var token = await _authService.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+        {
+            throw new AuthenticationException("Authentication failed. Failed to obtain a valid token from API.");
+        }
+
+        var url = $"{EndpointGetAdmins}";
+
+        _logger.LogInformation($"Requesting GetAdminsAsync");
+        
+        var response = await _httpRequestService.GetAsync<ApiResponse<GetAdminsResponse>>(url, token, cancellationToken);
+        if (response is { Success: true, Data: not null })
+        {
+            telegramBotAdmins = response.Data;
+        }
+        else
+        {
+            _logger.LogWarning($"Failed to get GetAdminsAsync: {response?.Message}");
+        }
+
+        if (response == null)
+        {
+            _logger.LogError("Failed to GetAdminsAsync.");
+        }
+
+        return telegramBotAdmins;
     }
 }
