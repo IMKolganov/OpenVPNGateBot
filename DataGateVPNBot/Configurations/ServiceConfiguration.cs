11908c53a19b38afa8e3a1cbbeedf0d0ed17d92a
Ivan Kolganov <imkolganov@gmail.com>
2025-07-17 01:25:44 +0300

    Enables HTTPS with Let's Encrypt
    Adds Let's Encrypt integration to automatically generate and renew SSL certificates.
    This allows the bot to be served over HTTPS, improving security and user experience.
    
    The changes include:
    - Adds Certes NuGet package for Let's Encrypt integration.
    - Implements a CertificateGenerator service to handle certificate requests and renewals.
    - Creates a LetsEncryptAccountStore to manage the ACME account key.
    - Configures the application URL to use the correct domain.

--- DataGateVPNBot/Configurations/ServiceConfiguration.cs
+++ DataGateVPNBot/Configurations/ServiceConfiguration.cs
@@ -1,10 +1,12 @@
-﻿using DataGateVPNBot.Handlers;
+﻿using Certes;
+using DataGateVPNBot.Handlers;
 using DataGateVPNBot.Services;
 using DataGateVPNBot.Services.BotServices;
 using DataGateVPNBot.Services.BotServices.Interfaces;
 using DataGateVPNBot.Services.DashboardServices;
 using DataGateVPNBot.Services.DashboardServices.Interfaces;
 using DataGateVPNBot.Services.Interfaces;
+using DataGateVPNBot.Services.LetsEncrypt;
 using Telegram.Bot.AspNetCore;
 using OvpnFileService = DataGateVPNBot.Services.BotServices.OvpnFileService;
 
@@ -14,6 +16,9 @@ public static class ServiceConfiguration
 {
     public static void ConfigureServices(this IServiceCollection services)
     {
+        services.AddSingleton<IKey>(_ => LetsEncryptAccountStore.LoadOrCreateAccountKey());
+        services.AddSingleton<CertificateGenerator>();
+        
         services.AddScoped<IIncomingMessageLogService, IncomingMessageLogService>();
         services.AddScoped<ITelegramBotUserService, TelegramBotUserService>();
         services.AddScoped<ILocalizationService, LocalizationService>();
